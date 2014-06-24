64e877f931bca559748ca07a24c5e3ffe4fcb09f
Kevin Wu <kevinpingwu2@gmail.com>
2014-06-24 22:25:07 +1000

    TinybeansLogonRedirectAutomatically

--- ConnectClient/src/main/java/com/yang/connect/client/ConnectClientImpl.java
+++ ConnectClient/src/main/java/com/yang/connect/client/ConnectClientImpl.java
@@ -28,25 +28,28 @@ import com.yang.connect.client.util.LogUtils;
 public class ConnectClientImpl implements ConnectClient {
 	private static String ENCODING_UTF_8 = "UTF-8";
 	private final static String USER_AGENT_MOZILLA5 = "Mozilla/5.0";
-	
+
 	private CookieManager cookieManager = new CookieManager();
-	
+
 	/**
 	 * Makes this class a singleton.
 	 */
-	private static ConnectClient connectClient = new ConnectClientImpl() ;
+	private static ConnectClient connectClient = new ConnectClientImpl();
+
 	public static ConnectClient getInstance() {
 		return connectClient;
 	}
-	
+
 	/**
-	 * Note: This constructor sets system-wide cookie handler SHARED BY ALL CONNECTIONS.
+	 * Note: This constructor sets system-wide cookie handler SHARED BY ALL
+	 * CONNECTIONS.
 	 */
 	private ConnectClientImpl() {
 		cookieManager = new CookieManager();
 		cookieManager.setCookiePolicy(CookiePolicy.ACCEPT_ORIGINAL_SERVER);
-		
-		// *************** Sets system-wide cookie handler SHARED BY ALL CONNECTIONS ****************
+
+		// *************** Sets system-wide cookie handler SHARED BY ALL
+		// CONNECTIONS ****************
 		CookieHandler.setDefault(cookieManager);
 	}
 
@@ -97,17 +100,17 @@ public class ConnectClientImpl implements ConnectClient {
 		LogUtils.info("*** download data ***");
 		LogUtils.debug("0. Show cookie store");
 		displayCookieStore();
-		
+
 		URL url = new URL(urlString);
 		HttpURLConnection con = (HttpURLConnection) url.openConnection();
 		con.setReadTimeout(20000 /* milliseconds */);
 		con.setConnectTimeout(5000 /* milliseconds */);
 		con.setRequestMethod("GET");
 		con.setDoInput(true);
-		
+
 		LogUtils.debug("1. Show cookie store");
 		displayCookieStore();
-		
+
 		// Starts the query
 		con.connect();
 		LogUtils.debug("2. Show cookie store");
@@ -119,42 +122,44 @@ public class ConnectClientImpl implements ConnectClient {
 		displayCookieStore();
 
 		InputStream is = con.getInputStream();
-		
+
 		LogUtils.debug("4. Show cookie store");
 		displayCookieStore();
-		
+
 		String responseString = parseInputStream(is);
 		return responseString;
 	}
-	
+
 	protected void displayCookieStore() {
 		CookieStore cookieStore = cookieManager.getCookieStore();
 		List<URI> urIs = cookieStore.getURIs();
 		displayURIs(urIs);
-		
+
 		List<HttpCookie> cookies = cookieStore.getCookies();
 		displayCookies(cookies);
 	}
-	
+
 	private void displayURIs(List<URI> urIs) {
 		for (URI uri : urIs) {
 			LogUtils.debug("URI:" + uri.toString());
 		}
 	}
-	
+
 	private void displayCookies(List<HttpCookie> cookies) {
 		for (HttpCookie cookie : cookies) {
 			LogUtils.debug("Cookie:" + cookie.toString());
 		}
 	}
-	
+
 	protected void handleCookies(HttpURLConnection con) {
-		/*Map<String, List<String>> headerFields = con.getHeaderFields();
-		List<String> cookieFeilds = headerFields.get("Set-Cookie");
-		
-		for (List<String> header : headerFields) {
-			
-		}*/
+		/*
+		 * Map<String, List<String>> headerFields = con.getHeaderFields();
+		 * List<String> cookieFeilds = headerFields.get("Set-Cookie");
+		 * 
+		 * for (List<String> header : headerFields) {
+		 * 
+		 * }
+		 */
 	}
 
 	@Override
@@ -167,178 +172,184 @@ public class ConnectClientImpl implements ConnectClient {
 			return msg;
 		}
 	}
-	
+
 	// Given a string representation of a URL, sets up a connection and gets
 	// an input stream.
-	/*protected InputStream sendPOST(String urlString, Map<String, Object> dataMap) throws IOException {
-		LogUtils.info(String.format("*** POST to URL %s ***", urlString));
-		LogUtils.debug("0. Show cookie store");
-		displayCookieStore();
-		
-		URL url = new URL(urlString);
-		HttpURLConnection con = (HttpURLConnection) url.openConnection();
-		con.setReadTimeout(20000  milliseconds );
-		con.setConnectTimeout(5000  milliseconds );
-		con.setRequestMethod("POST");
-		con.setDoInput(true);
-		
-		LogUtils.debug("1. Show cookie store");
-		displayCookieStore();
-		
-		// Starts the query
-		con.connect();
-		LogUtils.debug("2. Show cookie store");
-		displayCookieStore();
+	/*
+	 * protected InputStream sendPOST(String urlString, Map<String, Object>
+	 * dataMap) throws IOException {
+	 * LogUtils.info(String.format("*** POST to URL %s ***", urlString));
+	 * LogUtils.debug("0. Show cookie store"); displayCookieStore();
+	 * 
+	 * URL url = new URL(urlString); HttpURLConnection con = (HttpURLConnection)
+	 * url.openConnection(); con.setReadTimeout(20000 milliseconds );
+	 * con.setConnectTimeout(5000 milliseconds ); con.setRequestMethod("POST");
+	 * con.setDoInput(true);
+	 * 
+	 * LogUtils.debug("1. Show cookie store"); displayCookieStore();
+	 * 
+	 * // Starts the query con.connect();
+	 * LogUtils.debug("2. Show cookie store"); displayCookieStore();
+	 * 
+	 * int response = con.getResponseCode(); LogUtils.debug("The response is: "
+	 * + response); LogUtils.debug("3. Show cookie store");
+	 * displayCookieStore();
+	 * 
+	 * InputStream stream = con.getInputStream();
+	 * 
+	 * LogUtils.debug("4. Show cookie store"); displayCookieStore(); return
+	 * stream; }
+	 */
 
-		int response = con.getResponseCode();
-		LogUtils.debug("The response is: " + response);
-		LogUtils.debug("3. Show cookie store");
-		displayCookieStore();
-
-		InputStream stream = con.getInputStream();
-		
-		LogUtils.debug("4. Show cookie store");
-		displayCookieStore();
-		return stream;
-	}
-	*/
-	
-	// Ref: http://stackoverflow.com/questions/4205980/java-sending-http-parameters-via-post-method-easily
+	// Ref:
+	// http://stackoverflow.com/questions/4205980/java-sending-http-parameters-via-post-method-easily
 	protected String sendPOST2(String urlString, Map<String, Object> dataMap) throws IOException {
-		//By default, new instances of HttpCookie work only with servers that support RFC 2965 cookies. Many web servers support only the older specification, RFC 2109. For compatibility with the most web servers, set the cookie version to 0.
+		// By default, new instances of HttpCookie work only with servers that
+		// support RFC 2965 cookies. Many web servers support only the older
+		// specification, RFC 2109. For compatibility with the most web servers,
+		// set the cookie version to 0.
 		// cookie.setVersion(0);
-		
+
 		/*
-		 * Prior to Android 2.2 (Froyo), this class had some frustrating bugs. In particular, calling close() on a readable InputStream could poison the connection pool. Work around this by disabling connection pooling:
-		 * private void disableConnectionReuseIfNecessary() {
-			   // Work around pre-Froyo bugs in HTTP connection reuse.
-			   if (Integer.parseInt(Build.VERSION.SDK) < Build.VERSION_CODES.FROYO) {
-			     System.setProperty("http.keepAlive", "false");
-			   
-			 }}
-		*/
-		
+		 * Prior to Android 2.2 (Froyo), this class had some frustrating bugs.
+		 * In particular, calling close() on a readable InputStream could poison
+		 * the connection pool. Work around this by disabling connection
+		 * pooling: private void disableConnectionReuseIfNecessary() { // Work
+		 * around pre-Froyo bugs in HTTP connection reuse. if
+		 * (Integer.parseInt(Build.VERSION.SDK) < Build.VERSION_CODES.FROYO) {
+		 * System.setProperty("http.keepAlive", "false");
+		 * 
+		 * }}
+		 */
+
 		LogUtils.info(String.format("*** POST to URL %s ***", urlString));
 		LogUtils.debug("0. Show cookie store");
 		displayCookieStore();
-		
+
 		final String encoding = ENCODING_UTF_8;
-		
+
 		// prepare Test data
 		String postData = getPostData(dataMap, encoding);
-        byte[] postDataBytes = postData.getBytes(encoding);
-        
-        URL url = new URL(urlString);
-        HttpURLConnection conn = (HttpURLConnection)url.openConnection();
-        // URL.openConnection(Proxy) 
-        
-        conn.setDoInput(true);
-        conn.setDoOutput(true);
-        
-        conn.setReadTimeout(20000 /* milliseconds */);
-        conn.setConnectTimeout(5000 /* milliseconds */);
-        conn.setRequestMethod("POST");
-        conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-        conn.setRequestProperty("Content-Length", String.valueOf(postDataBytes.length));
-        conn.setRequestProperty("User-Agent", USER_AGENT_MOZILLA5);
+		byte[] postDataBytes = postData.getBytes(encoding);
+
+		URL url = new URL(urlString);
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		// URL.openConnection(Proxy)
+
+		conn.setDoInput(true);
+		conn.setDoOutput(true);
+
+		conn.setReadTimeout(20000 /* milliseconds */);
+		conn.setConnectTimeout(5000 /* milliseconds */);
+		conn.setRequestMethod("POST");
+		conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+		conn.setRequestProperty("Content-Length", String.valueOf(postDataBytes.length));
+		conn.setRequestProperty("User-Agent", USER_AGENT_MOZILLA5);
 		conn.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
-		
+
 		// what are they?
-		conn.setInstanceFollowRedirects(false); 
-		conn.setUseCaches (false);
-        
-        
-        conn.getOutputStream().write(postDataBytes);
-        
-        int responseCode = conn.getResponseCode();
+		final boolean autoRedirect = true;
+		conn.setInstanceFollowRedirects(autoRedirect);
+		conn.setUseCaches(false);
+
+		conn.getOutputStream().write(postDataBytes);
+
+		int responseCode = conn.getResponseCode();
 		LogUtils.debug("The response is: " + responseCode);
-		
-		
+
 		LogUtils.debug("3. Show cookie store");
 		displayCookieStore();
-        
-        InputStream is = conn.getInputStream();
+
+		InputStream is = conn.getInputStream();
 		String responseString = parseInputStream(is);
-		
-		//if (responseCode>=300 && responseCode<400) {
-		if (responseCode == 302 || responseCode == 303) {
-			//TODO - the response code check needs to be re-written carefully
-			String originalHost = url.getHost();
-			String respondedHost = conn.getURL().getHost();
-			//if (originalHost.equals(respondedHost)) {
-			       // we were redirected! Kick the user out to the browser to sign on?
-				LogUtils.info(String.format("******* WE ARE REDIRECTED from %s to %s. ********* ", originalHost, respondedHost));
-				
-				String newLocation = conn.getHeaderField( "Location" );
-				LogUtils.info(String.format("Redirected to %s", newLocation));
-				
-				String redirectedResponseHtml = sendGET(newLocation);
-				
-				LogUtils.info(String.format("Redirected done! Returned: %s", redirectedResponseHtml));
-				LogUtils.debug("4. Show cookie store AFTER POST REDIRECTION.");
-				displayCookieStore();
+
+		// do you want to manually redirect if responseCode is 3XX?
+		if (!autoRedirect) {
+			redirectManually(responseCode, url, conn);
 		}
 
 		return responseString;
 	}
-	
+
+	private void redirectManually(int responseCode, URL url, HttpURLConnection conn) throws IOException {
+		if (responseCode == 302 || responseCode == 303) {
+			// TODO - the response code check needs to be re-written carefully
+			String originalHost = url.getHost();
+			String respondedHost = conn.getURL().getHost();
+			// if (originalHost.equals(respondedHost)) {
+			// we were redirected! Kick the user out to the browser to sign on?
+			LogUtils.info(String.format("******* WE ARE REDIRECTED from %s to %s. ********* ", originalHost,
+					respondedHost));
+
+			String newLocation = conn.getHeaderField("Location");
+			LogUtils.info(String.format("Redirected to %s", newLocation));
+
+			String redirectedResponseHtml = sendGET(newLocation);
+
+			LogUtils.info(String.format("Redirected done! Returned: %s", redirectedResponseHtml));
+			LogUtils.debug("4. Show cookie store AFTER POST REDIRECTION.");
+			displayCookieStore();
+		}
+	}
+
 	/**
-	 * Converts parameter map to post data string with specified encoding, which is ready to send over wire via http POST.
-	 * Note: the encoding is handled as UTF-8.
+	 * Converts parameter map to post data string with specified encoding, which
+	 * is ready to send over wire via http POST. Note: the encoding is handled
+	 * as UTF-8.
 	 * 
 	 * @param params
 	 * @return encoded post data string with specified encoding
 	 * @throws UnsupportedEncodingException
 	 */
-	private String getPostData(Map<String,Object> params, String encoding) throws UnsupportedEncodingException {
-        StringBuilder postData = new StringBuilder();
-        for (Map.Entry<String,Object> param : params.entrySet()) {
-            if (postData.length() != 0) postData.append('&');
-            postData.append(URLEncoder.encode(param.getKey(), encoding));
-            postData.append('=');
-            postData.append(URLEncoder.encode(String.valueOf(param.getValue()), encoding));
-        }
-        
-        return postData.toString();
+	private String getPostData(Map<String, Object> params, String encoding) throws UnsupportedEncodingException {
+		StringBuilder postData = new StringBuilder();
+		for (Map.Entry<String, Object> param : params.entrySet()) {
+			if (postData.length() != 0)
+				postData.append('&');
+			postData.append(URLEncoder.encode(param.getKey(), encoding));
+			postData.append('=');
+			postData.append(URLEncoder.encode(String.valueOf(param.getValue()), encoding));
+		}
+
+		return postData.toString();
 	}
-	
+
 	// TO BE DELETED
 	protected void sendPOSTV3(String urlString, Map<String, String> dataMap) throws IOException {
 		URL url = new URL(urlString);
 		HttpsURLConnection con = (HttpsURLConnection) url.openConnection();
- 
-		//add reuqest header
+
+		// add reuqest header
 		con.setRequestMethod("POST");
 		con.setRequestProperty("User-Agent", USER_AGENT_MOZILLA5);
 		con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
- 
+
 		String urlParameters = "sn=C02G8416DRJM&cn=&locale=&caller=&num=12345";
- 
+
 		// Send post request
 		con.setDoOutput(true);
 		DataOutputStream wr = new DataOutputStream(con.getOutputStream());
 		wr.writeBytes(urlParameters);
 		wr.flush();
 		wr.close();
- 
+
 		int responseCode = con.getResponseCode();
 		System.out.println("\nSending 'POST' request to URL : " + url);
 		System.out.println("Post parameters : " + urlParameters);
 		System.out.println("Response Code : " + responseCode);
- 
-		BufferedReader in = new BufferedReader(
-		        new InputStreamReader(con.getInputStream()));
+
+		BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
 		String inputLine;
 		StringBuffer response = new StringBuffer();
- 
+
 		while ((inputLine = in.readLine()) != null) {
 			response.append(inputLine);
 		}
 		in.close();
- 
-		//print result
+
+		// print result
 		System.out.println(response.toString());
- 
+
 	}
 
 }
